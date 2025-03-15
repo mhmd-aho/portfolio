5f78429ff919bf4858da6affbbb64968e7428d60
mhmd-aho <mhmdabouhamoud@gmail.com>
2025-03-15 17:20:03 +0200

    portfolio v2

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-    let img = document.getElementById('myPic');
-    let info = document.getElementById('info');
-    img.onmouseover = () => {
-        info.style.visibility = "visible";
-        img.style.filter = 'grayscale(0)'
-    }
-    img.onmouseout = () =>{
-      info.style.visibility = "hidden"
-    }
+let icons = document.getElementsByClassName("logo");
