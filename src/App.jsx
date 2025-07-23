6017e722752c43c54985e05ae17f6dcf87903f18
mhmd-aho <mhmdabouhamoud@gmail.com>
2025-07-23 23:20:22 +0300

    fix 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { Suspense, lazy } from 'react';
+import Header from './header';
+import Loading from './loading';
 const Home = lazy(() => import('./home/home'));
 const AboutMe = lazy(() => import('./about/aboutMe'));
 const ShowCase = lazy(() => import('./showcase/showCase'));
 function App() {
   return (
-    <div className="overflow-hidden "> 
+  <div className="overflow-hidden "> 
     <Header />
     <main className="pt-12">
-      <Home />
-      <AboutMe />
-      <ShowCase />
+      <Suspense fallback={<Loading />}>
+          <Home />
+          <AboutMe />
+          <ShowCase />
+      </Suspense>
     </main>
-    </div>
+  </div>
   )
 }
 
